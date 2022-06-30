521e3ff8b9887799991c59060ba7198debaad99c
Sawan7588 <105876636+Sawan7588@users.noreply.github.com>
2022-06-30 18:25:51 +0530

    Add files via upload

---
+++ Programming_Assignment_1.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Q.1 Write a Python program to print"Hello Python"?
+Ans :- print("Hello Python")
+
+
+# In[ ]:
+
+
+# Q.2 Write a Python program to do arithmetical operations addition and division.?
+Ans:-
+    a = 6
+    b = 3
+    add = a+b
+    div = a/b
+    print("addition of numbers : ",add)
+    print("division of numbers : ",div)
+    
+    
+    
+
+
+# In[ ]:
+
+
+# Q.3 Write a Python program to find the area of a triangle?
+Ans :-
+    
+    b = 4
+    h = 5
+    area = (b*h)/2
+    print("Area of triangle is : ", area)
+
+
+# In[ ]:
+
+
+# Q.4 Write a Python program to swap two variables?
+Ans:-
+    
+    x = 10
+    y = 20
+    
+    x,y = y,x
+    
+    print("Value of x :",x)
+    print("Value of y :",y)
+    
+
+
+# In[ ]:
+
+
+# Q.5 Write a Python program to generate a random number?
+Ans :- 
+    
+    import random
+    n = random.random()
+    print(n)
+
